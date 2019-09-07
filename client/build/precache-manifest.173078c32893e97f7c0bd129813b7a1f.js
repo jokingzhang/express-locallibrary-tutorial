11642fdb5230255ff24e2eef912639b7550d296f
zhangbx <496691544@qq.com>
2019-09-07 14:21:50 +0800

    add build

---
+++ client/build/precache-manifest.173078c32893e97f7c0bd129813b7a1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d6d6ef1efa2bc7c58fcdb45449917b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca9d74afb6d20906e2cb",
+    "url": "/static/css/2.8971d352.chunk.css"
+  },
+  {
+    "revision": "32df682d43c31937f350",
+    "url": "/static/css/main.91beb6df.chunk.css"
+  },
+  {
+    "revision": "ca9d74afb6d20906e2cb",
+    "url": "/static/js/2.a159f40b.chunk.js"
+  },
+  {
+    "revision": "32df682d43c31937f350",
+    "url": "/static/js/main.3ed5a01e.chunk.js"
+  },
+  {
+    "revision": "83e7f8b54e61b1cbddf8",
+    "url": "/static/js/runtime~main.e38743dc.js"
+  }
+]);
